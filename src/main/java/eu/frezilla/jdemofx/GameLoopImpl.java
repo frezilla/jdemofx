f1a4894e4bc2050e5a41e3a6646f0e7d4347d144
fr3zill@ <fb.info@mailo.com>
2026-03-03 00:18:36 +0100

    Implémentation basique de la boucle du jeu

---
+++ src/main/java/eu/frezilla/jdemofx/GameLoopImpl.java
@@ -0,0 +1,36 @@
+package eu.frezilla.jdemofx;
+
+import eu.frezilla.jdemofx.loop.GameLoop;
+
+public class GameLoopImpl extends GameLoop {
+
+    @Override
+    public void cleanUpAndExit() {
+        }
+
+    @Override
+    public boolean exitLoop() {
+        return false;
+        }
+
+    @Override
+    public void initializeGameState() {
+        }
+
+    @Override
+    public void processInput() {
+        }
+
+    @Override
+    public void renderFrame() {
+        }
+
+    @Override
+    public void startGame() {
+        }
+
+    @Override
+    public void updateGameState() {
+        }
+    
+}
