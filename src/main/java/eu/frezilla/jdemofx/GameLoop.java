7ba8555c55a4f82d5b87483e6bfffa83aa028361
fr3zill@ <fb.info@mailo.com>
2026-03-02 23:57:34 +0100

    Gestion des logs

--- src/main/java/eu/frezilla/jdemofx/GameLoop.java
+++ src/main/java/eu/frezilla/jdemofx/GameLoop.java
@@ -1,6 +1,11 @@
 package eu.frezilla.jdemofx;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class GameLoop {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(GameLoop.class);
     
     public abstract void cleanUpAndExit();
     
@@ -13,13 +18,26 @@ public abstract class GameLoop {
     public abstract void renderFrame();
     
     public final void start() {
+        LOGGER.trace("start game");
         startGame();
+        
+        LOGGER.trace("intialize game");
         initializeGameState();
+        
+        LOGGER.trace("start loop");
         while(!exitLoop()) {
+            LOGGER.trace("process input");
             processInput();
+            
+            LOGGER.debug("update game state");
             updateGameState();
+            
+            LOGGER.debug("renfer frame");
             renderFrame();
         }
+        LOGGER.trace("exit loop");
+        
+        LOGGER.trace("cleanup and exot");
         cleanUpAndExit();
     }
     
