6a4c4d6afff960dc5be225d3d2cab1541f4e6037
fr3zill@ <34129644+frezilla@users.noreply.github.com>
2026-03-03 13:54:32 +0100

    Création de l'énumération des FPS

---
+++ src/main/java/eu/frezilla/jdemofx/loop/FrameRates.java
@@ -0,0 +1,26 @@
+package eu.frezilla.jdemofx.loop;
+
+public enum FrameRates {
+    FPS24(24),
+    FPS30(30),
+    FPS60(60),
+    FPS90(90),
+    FPS120(120),
+    FPS240(240);
+    
+    private final double duration;
+    private final int fps;
+    
+    private FrameRates(int fps) {
+        this.fps = fps;
+        this.duration = 1.0/fps;
+    }
+    
+    public double getDuration() {
+        return duration;
+    }
+    
+    public int getFps() {
+        return fps;
+    }
+}
