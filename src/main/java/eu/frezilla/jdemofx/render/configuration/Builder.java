c9ae22069bd668fdd09be12049be73879204a013
fr3zill@ <34129644+frezilla@users.noreply.github.com>
2026-03-11 13:43:59 +0100

    Création des classes de configuration de l'affichage

---
+++ src/main/java/eu/frezilla/jdemofx/render/configuration/Builder.java
@@ -0,0 +1,80 @@
+package eu.frezilla.jdemofx.render.configuration;
+
+import eu.frezilla.jdemofx.render.ImageType;
+import eu.frezilla.jdemofx.render.Size;
+import eu.frezilla.jdemofx.render.Sizes;
+import java.awt.Color;
+
+/**
+ * La classe {@code Builder} permet de créer une instance de la classe
+ * {@Configuration}.
+ *
+ * <p>
+ * Les valeurs par défaut sont les suivantes :
+ * <ul>
+ * <li>Couleur de l'arrière plan : {@link Color#black}</li>
+ * <li>Type de l'image : {@link ImageType#TYPE_INT_RGB}</li>
+ * <li>Taille : {@link Sizes#HIGH}</li>
+ * </ul>
+ *
+ * @see Configuration
+ * @see Sizes
+ */
+public final class Builder {
+
+    private Color backgroundColor;
+    private ImageType imageType;
+    private Size size;
+
+    Builder() {
+        backgroundColor = Color.black;
+        imageType = ImageType.TYPE_INT_RGB;
+        size = Sizes.HIGH.getSize();
+    }
+
+    /**
+     * Créé une nouvelle {@code Configuration} à partir des paramètres définis.
+     *
+     * @return Nouvelle instance de la classe {@code Configuration}
+     * @see Configuration
+     */
+    public Configuration build() {
+        return new Configuration(backgroundColor, imageType, size);
+    }
+    
+    /**
+     * Défini la couleur de l'arrière plan.
+     * 
+     * @param color Couleur
+     * @return {@code Builder} courant
+     * @see Color
+     */
+    public Builder backgroundColor(Color color) {
+        this.backgroundColor = color;
+        return this;
+    }
+    
+    /**
+     * Définit le type de l'image.
+     * 
+     * @param imageType Type de l'image
+     * @return {@code Builder} courant
+     * @see ImageType
+     */
+    public Builder imageType(ImageType imageType) {
+        this.imageType = imageType;
+        return this;
+    }
+
+    /**
+     * Définit la taille.
+     *
+     * @param size Taille
+     * @return {@code Builder} courant
+     * @see Size
+     */
+    public Builder size(Size size) {
+        this.size = size;
+        return this;
+    }
+}
---
+++ src/main/java/eu/frezilla/jdemofx/render/configuration/Configuration.java
@@ -0,0 +1,79 @@
+package eu.frezilla.jdemofx.render.configuration;
+
+import eu.frezilla.jdemofx.render.ImageType;
+import eu.frezilla.jdemofx.render.Size;
+import java.awt.Color;
+import java.util.Objects;
+
+/**
+ * La class {@code Configuration} stocke les informations nécessaires à la
+ * configuration de la zone d'affichage.
+ *
+ * <p>
+ * Les paramètres de la {@code Configuration} sont les suivants :
+ * <ul>
+ * <li>Le type de l'image (valeur non null)</li>
+ * <li>La taille (valeur non nulle)</li>
+ * <li>La couleur d'arrière plan</li>
+ * </ul>
+ *
+ * <p>
+ * Une {@code Configuration} est immuable; elle est créée via l'utilisation d'un
+ * {@code Builder} créé par l'appel à la méthode
+ * {@link Configuration#getBuilder()}.
+ *
+ * @see Builder
+ */
+public final class Configuration {
+
+    private final Color backgroundColor;
+    private final ImageType imageType;
+    private final Size size;
+
+    Configuration(Color backgroundColor, ImageType imageType, Size size) {
+        this.backgroundColor = Objects.requireNonNull(backgroundColor, "La couleur de l'arrière plan n'est pas correctement définie");
+        this.imageType = Objects.requireNonNull(imageType, "Le type de l'image n'est pas correctement défini");
+        this.size = Objects.requireNonNull(size, "la taille n'est pas correctement définie");
+    }
+    
+    /**
+     * Retourne la couleur de fond de la zone d'affichage.
+     * 
+     * @return Couleur du fond
+     * @see Color
+     */
+    public Color getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    /**
+     * Créé une instance {@code Builder} nécessaire à la création d'une
+     * {@code Configuration}.
+     *
+     * @return {@code Builder}
+     * @see Builder
+     */
+    public static Builder getBuilder() {
+        return new Builder();
+    }
+    
+    /**
+     * Retourne le type de l'image.
+     * 
+     * @return Type de l'image
+     * @see ImageType
+     */
+    public ImageType getImageType() {
+        return imageType;
+    }
+
+    /**
+     * Retourne la taille configuré.
+     *
+     * @return Taille
+     * @see Size
+     */
+    public Size getSize() {
+        return size;
+    }
+}
